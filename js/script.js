4a9fbe8a41d89aebf34571d5e85f5ff49eb73b36
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-12-01 19:40:43 +0000

    .

--- js/script.js
+++ js/script.js
@@ -17,5 +17,6 @@ if (navigator.serviceWorker) {
  * this function
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  document.getElementById("calculate").innerHTML = 
+
 }
